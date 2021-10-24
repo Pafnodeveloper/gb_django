5317fab6c37767d6d341c6e2a6b4d2af9d6ed787
PafnoDeveloper <pavelrodin1089@gmail.com>
2021-10-24 09:01:00 +0300

    added errors and messages

--- users/validators.py
+++ users/validators.py
@@ -3,6 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 def validate_email(value):
-    if not ("email.com" in value):
+    if not ("email@ivan.com" in value):
         raise ValidationError(_("Not a valid email. Try one more time"))
     return value
